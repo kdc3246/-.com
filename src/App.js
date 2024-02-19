520fb7473543009e8fdd89ccbc3ce84ca3b84499
kdc119 <skt32@naver.com>
2024-02-19 18:28:07 +0900

    라우팅 및 컴포넌트 셋팅

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import HomeView from "./views/HomeView";
-
 const App = () => {
-    return (
+  return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomeView />} />
+            <Route path="/about" element={<AboutView />} />
         </Routes>
     </BrowserRouter>
-    );
-};
+  )
+}
 
 export default App;
